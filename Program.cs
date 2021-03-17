c1ae2d6e810d6b510f86aec4d6cede4fbf7aa9c0
Kaylany Paulus <kaylanysilvapaulus@gmail.com>
2021-03-17 20:56:33 -0300

    Contagem Regressiva- Burn it.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("CONTAGEM REGRESSIVA");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Preparar... \n");
+            Console.ResetColor();
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Apontar... \n");
+            Console.ResetColor();
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("FOGO!");Console.ResetColor();Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
+        
+    
